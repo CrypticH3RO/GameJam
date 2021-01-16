b14caa8c38d0431fe52e475174c3ba999c334b01
CrypticH3RO <aharoon179@hotmail.com>
2021-01-16 14:55:16 -0500

    added level 1 prototype

---
+++ GameJam1/Assets/Scripts/ScriptsAamir/ScoreManagerAA.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManagerAA : MonoBehaviour
+{
+    public int playerScore = 0;
+
+    public UpdatePlayerInfo UpdatePlayerInfo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdatePlayerInfo = GameObject.FindObjectOfType<UpdatePlayerInfo>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateScore()
+    {
+        playerScore += 1;
+        UpdatePlayerInfo.UpdateScoreText(playerScore);
+    }
+}
