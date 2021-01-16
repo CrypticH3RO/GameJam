faac88ac19cb74e360083bbf98034d937e81d7c3
CrypticH3RO <aharoon179@hotmail.com>
2021-01-16 12:45:03 -0500

    adding tutorial resources

---
+++ GameJam1/Assets/Scripts/Move.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    private float _userHorizontalInput;
+    private const float ScaleMovement = 0.25f;
+    private Transform playerTransform;
+    private float _userRotInput;
+    private Vector3 _userRot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = gameObject.GetComponent<Transform>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        // vertical is up and down
+        _userHorizontalInput = Input.GetAxis("Vertical");
+        _userRotInput = Input.GetAxis("Horizontal");
+        Debug.Log(_userHorizontalInput);
+        // by default you get the cartian? 5:30
+        // eularAngles are the x y z angles
+        _userRot = playerTransform.rotation.eulerAngles;
+        // vector 3 is just an object that captures the 3 dimensional inputs of any sorts. 
+        _userRot += new Vector3(0, _userRotInput, 0);
+
+        // have to convert to Eular coordinates because playerTransforms.rotation expects a cartian angle
+        playerTransform.rotation = Quaternion.Euler(_userRot);
+        //playerTransform.rotation = Quaternion.Eular(0,0,0);
+        playerTransform.position += transform.forward * _userHorizontalInput * ScaleMovement;
+
+    }
+}
---
+++ GameJam1/Assets/Scripts/UpdatePlayerInfo.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // wasn't included in the tutorial video
+public class UpdatePlayerInfo : MonoBehaviour
+{
+    // Set this gameobject from the Scene menu by dragging and dropping
+    public GameObject playerScoreObj;
+    private GameObject _playerLivesObj; //underscore infront of private variables convention
+    public GameObject GameOverText;
+
+
+    private Text _playerScoreText;
+    private Text _playerLivesText;
+
+    public SpawnCharacter SpawnCharacter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameOverText.SetActive(false);
+
+        _playerScoreText = playerScoreObj.GetComponent<Text>();
+        _playerScoreText.text = "Score: 0";
+
+        SpawnCharacter = GameObject.FindObjectOfType<SpawnCharacter>();
+        // Could do this in one line the first 2
+        _playerLivesObj = GameObject.Find("Lives");
+        _playerLivesText = _playerLivesObj.GetComponent<Text>();
+        _playerLivesText.text = "Num of Lives: " + SpawnCharacter.NumOfLives;
+    }
+
+    public void UpdateScoreText(int score)
+    {
+        _playerScoreText.text = "Score: " + score;
+    }
+
+    public void UpdatePlayerLivesText(int lives)
+    {
+        _playerLivesText.text = "Num of lives: " + lives;
+    }
+
+    public void DisplayGameOver()
+    {
+        GameOverText.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
